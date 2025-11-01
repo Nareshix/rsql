9f0fbf0a3fb7eb28fb6482a495b5559b829c854c
Nareshix <nareshix66@gmail.com>
2025-11-01 15:44:51 +0800

    added prepare statement, havent yet the binding logic

--- src/prepare.rs
+++ src/prepare.rs
@@ -1,6 +1,6 @@
-use libsqlite3_sys as ffi;
+use libsqlite3_sys::{self as ffi, sqlite3_stmt};
 
-use crate::connection::Connection;
+use crate::{connection::Connection, error::Error, utils::get_sqlite_error_msg};
 use std::{
     ffi::{CStr, CString,},
     ptr,
@@ -8,49 +8,34 @@ use std::{
 
 #[allow(dead_code)]
 pub struct Statement {
-    stmt: *mut ffi::sqlite3_stmt,
+    stmt: *mut sqlite3_stmt,
 }
 
-//maybe dont add drop trait
-
-
-//*ppStmt is left pointing to a compiled prepared statement that can be executed using sqlite3_step(). 
-// If there is an error, *ppStmt is set to NULL. If the input text contains no SQL
-// (if the input is an empty string or a comment) then *ppStmt is set to NULL.
-// The calling procedure is responsible for deleting the compiled SQL statement 
-//using sqlite3_finalize() after it has finished with it. ppStmt may not be NULL.
-
-//Bind any parameters if needed (sqlite3_bind_*()).
-//Execute the statement, usually in a loop with sqlite3_step().
-//Process the results.
-//You MUST call sqlite3_finalize(ppStmt) to delete the statement and release the memory. Failing to do this will cause a resource leak.
 
 #[allow(dead_code)]
 impl Connection {
-    fn prepare(&self, sql_query: &str) -> Result<Statement, String> {
-        let c_sql_query = CString::new(sql_query).expect("CString::new failed");
+    pub fn prepare(&self, sql: &str) -> Result<Statement, Error> {
+        let c_sql_query = CString::new(sql).expect("CString::new failed");
         let mut stmt = ptr::null_mut();
         let code = unsafe {
             ffi::sqlite3_prepare_v2(
-                self.handle,
+                self.get_db(),
                 c_sql_query.as_ptr(),
                 -1,
                 &mut stmt,
                 ptr::null_mut(),
             )
         };
+
         if code == ffi::SQLITE_OK {
             Ok(Statement { stmt })
         } else {
-            unsafe {
-                // sqlite says we should not  drop c_error_msg
-                let c_error_msg = ffi::sqlite3_errstr(code);
-                let safe_error_msg = CStr::from_ptr(c_error_msg);
-                let error_msg = safe_error_msg.to_string_lossy().into_owned();
 
-                // ffi::sqlite3_close(db_handle);
-                Err(error_msg)
-            }
+            let error_msg = unsafe {
+                get_sqlite_error_msg(self.get_db())
+            };
+            Err(Error::SqliteFailiure(error_msg))
+
         }
     }
 }
