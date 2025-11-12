7e9a49426969733dd769a9dbe6d05aef414758c7
Nareshix <nareshix66@gmail.com>
2025-11-12 14:15:58 +0800

    test for execute!() macro

--- test_macro/src/main.rs
+++ test_macro/src/main.rs
@@ -1,10 +1,20 @@
 use macros::execute;
+use rsql::internal_sqlite::connection::Connection;
 
 
 #[derive(PartialEq, Debug)]
+#[allow(unused)]
 struct User;
 
-fn main() {
-    let x = "ads".to_string();
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let conn = Connection::open("hi.db")?;
+    let name = "mom";
+    let email = "mom@example.com";
 
+
+
+
+    execute!(conn, "INSERT INTO users (username, email) VALUES (?, ?)", (name, email));
+
+    Ok(())
 }
