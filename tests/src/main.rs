a448c1a64e74e255d4389b4267d7d6684e43d0ab
Nareshix <nareshix66@gmail.com>
2025-11-13 12:24:53 +0800

    created main.rs

---
+++ tests/src/main.rs
@@ -0,0 +1,20 @@
+use macros::SqlMapping;
+use rsql::internal_sqlite::connection::Connection;
+
+
+#[derive(Debug, SqlMapping)]
+struct Person {
+    id: i32,
+    username: String,
+    email: String,
+}
+
+fn main() {
+    let conn = Connection::open("hi.db").unwrap();
+
+    let statement = conn.prepare("SELECT * FROM users").unwrap();
+
+    for person in statement.query(Person) {
+        println!("Found user: {:?}", person);
+    }
+}
