8dd8d9d2cacaf664fe9eebeac305b2e5365a2a20
Nareshix <nareshix66@gmail.com>
2025-11-16 09:42:22 +0800

    some simple testing for speed

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -7,7 +7,7 @@ struct Person {
 use std::time::Instant;
 
 
-// results
+// results for select * from table
 // rsql Elapsed: 136.88s
 // rusqlite Elapsed: 137.13s
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -16,8 +16,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let now = Instant::now();
 
-    let statement = conn.prepare("SELECT * FROM wukong_data")?;
-    for person in statement.query(Person) {}
+    let statement = conn.prepare("SELECT * FROM wukong_data WHERE url LIKE '%gss0%'")?;
+    for person in statement.query(Person) {
+        // println!("{:?}", person);
+    }
 
     let elapsed_rsql = now.elapsed();
     // endrsql
@@ -26,7 +28,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let now = Instant::now();
 
     let conn = rusqlite::Connection::open("wukong.db")?;
-    let mut stmt = conn.prepare("SELECT * FROM wukong_data")?;
+    let mut stmt = conn.prepare_cached("SELECT * FROM wukong_data WHERE url LIKE '%gss0%'")?;
     let person_iter = stmt.query_map([], |row| {
         Ok(Person {
             url: row.get(0)?,
@@ -34,7 +36,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         })
     })?;
 
-    for person in person_iter {}
+    for person in person_iter {
+        // println!("{:?}", person.unwrap())
+    }
 
     let elapsed_rusqlite = now.elapsed();
     //endrusqlite
