74bb8a11be7921062b85eb529f747810bd6ad67d
Nareshix <nareshix66@gmail.com>
2025-11-16 11:40:54 +0800

    moved connectionallocation field as standalone into sqliteopenerrors enum

--- rsql-core/src/utility/error.rs
+++ rsql-core/src/utility/error.rs
@@ -1,12 +1,5 @@
 use std::ffi::c_int;
 
-/// This error occurs when SQLite is unable to allocate memory to hold
-/// the database connection object. In other words, the device where this porgram is running does not
-/// have enough ram
-#[derive(thiserror::Error, Debug)]
-#[error("SQLite is unable to allocate memory to hold the database connection object")]
-pub struct ConnectionAllocationFailed;
-
 /// recommended to **log** this error rather than directly handling it
 #[derive(thiserror::Error, Debug)]
 #[error("SQLite error {code}: {error_msg}")]
@@ -15,11 +8,16 @@ pub struct SqliteFailure {
     pub error_msg: String,
 }
 
+// This errors
 #[derive(thiserror::Error, Debug)]
 pub enum SqliteOpenErrors {
+    /// This error occurs when SQLite is unable to allocate memory to hold
+    /// the database connection object. In other words, the device where this porgram is running does not
+    /// have enough ram
     #[error("SQLite is unable to allocate memory to hold the database connection object")]
     ConnectionAllocationFailed,
 
+    // in case of any other errors
     #[error("SQLite error {code}: {error_msg}")]
     SqliteFailure { code: c_int, error_msg: String },
 }
