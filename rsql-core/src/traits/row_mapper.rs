4e279478d5a0f2a7d4839ed09d4f3c5aa043be0c
Nareshix <nareshix66@gmail.com>
2025-11-16 09:42:11 +0800

    added safety

--- rsql-core/src/traits/row_mapper.rs
+++ rsql-core/src/traits/row_mapper.rs
@@ -3,8 +3,7 @@ use libsqlite3_sys::sqlite3_stmt;
 pub trait RowMapper {
     type Output;
 
-    /// # Unsafe
+    /// # Safety
     /// The caller must ensure the statement has a row ready to be read.
-    #[allow(clippy::missing_safety_doc)]
     unsafe fn map_row(&self, stmt: *mut sqlite3_stmt) -> Self::Output;
 }
