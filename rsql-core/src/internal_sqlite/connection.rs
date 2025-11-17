7d2e0a80079301caebc062bace02187909d6d418
Nareshix <nareshix66@gmail.com>
2025-11-17 21:08:53 +0800

    made erorr handling more explicit for nulbyte in filename

--- rsql-core/src/internal_sqlite/connection.rs
+++ rsql-core/src/internal_sqlite/connection.rs
@@ -1,5 +1,6 @@
 use libsqlite3_sys::{
-    self as ffi, SQLITE_OK, SQLITE_OPEN_CREATE, SQLITE_OPEN_MEMORY, SQLITE_OPEN_READWRITE, sqlite3, sqlite3_busy_timeout,
+    self as ffi, SQLITE_OK, SQLITE_OPEN_CREATE, SQLITE_OPEN_MEMORY, SQLITE_OPEN_READWRITE, sqlite3,
+    sqlite3_busy_timeout,
 };
 use std::{
     ffi::{CString, c_int},
@@ -41,7 +42,9 @@ impl Connection {
         let mut db = ptr::null_mut();
 
         let c_filename =
-            CString::new(filename).map_err(|_| SqliteOpenErrors::EmbeddedNullInFileName)?;
+            CString::new(filename).map_err(|_| SqliteOpenErrors::EmbeddedNullInFileName {
+                filename: filename.to_owned(),
+            })?;
 
         let code = unsafe { ffi::sqlite3_open_v2(c_filename.as_ptr(), &mut db, flag, ptr::null()) };
 
@@ -51,8 +54,8 @@ impl Connection {
             };
             Err(SqliteOpenErrors::ConnectionAllocationFailed)
         } else if code == SQLITE_OK {
-            //TODO sqlite3_busy_timeout does return an int. It is nearly a gurantee for this 
-            // function to never fail. but its still good to handle it. 
+            //TODO sqlite3_busy_timeout does return an int. It is nearly a gurantee for this
+            // function to never fail. but its still good to handle it.
             unsafe { sqlite3_busy_timeout(db, 5000) };
             Ok(Connection { db })
         } else {
