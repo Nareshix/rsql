877bce2eee13ff1f4b0274db8381f063f94d440c
Nareshix <nareshix66@gmail.com>
2025-11-20 19:31:54 +0800

    made error handling a tad bit more robust

--- rsql-core/src/internal_sqlite/row.rs
+++ rsql-core/src/internal_sqlite/row.rs
@@ -1,4 +1,4 @@
-use libsqlite3_sys::{SQLITE_BUSY, SQLITE_ERROR, SQLITE_ROW, sqlite3_step};
+use libsqlite3_sys::{SQLITE_BUSY, SQLITE_DONE, SQLITE_ERROR, SQLITE_ROW, sqlite3_step};
 
 use crate::{
     errors::row::RowMapperError, internal_sqlite::statement::Statement,
@@ -24,11 +24,12 @@ impl<'a, M: RowMapper> Iterator for Rows<'a, M> {
             Some(Ok(item))
         } else if result_code == SQLITE_BUSY {
             Some(Err(RowMapperError::SqliteBusy))
-        } else if result_code == SQLITE_ERROR {
+        } else if result_code == SQLITE_DONE {
+            None
+        } else {
             let (code, error_msg) = unsafe { get_sqlite_failiure(self.stmt.conn.db) };
             Some(Err(RowMapperError::SqliteFailure { code, error_msg }))
-        } else {
-            None
+
         }
     }
 }
