1599456eb66753d267065bf7c05d587265c85416
Nareshix <nareshix66@gmail.com>
2025-11-22 15:47:32 +0800

    made the connecion from lazy_conection public whilst keeping the normal Connection private

--- rsql/src/lib.rs
+++ rsql/src/lib.rs
@@ -1,4 +1,4 @@
 pub use rsql_core::*;
-pub use rsql_core::internal_sqlite::connection::Connection;
+pub use rsql_core::internal_sqlite::lazy_connection::Connection;
 pub use macros::*;
 pub use rsql_core::internal_sqlite::lazy_statement::LazyStmt;
