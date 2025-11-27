90ca621f2e3f7b6044b851fe8318acaa93aa3dbd
Nareshix <nareshix66@gmail.com>
2025-11-27 23:23:24 +0800

    added fn where it infers types form sql statements. TODO wildcard

--- type_inference/src/main.rs
+++ type_inference/src/main.rs
@@ -1,20 +1,22 @@
+use crate::select_patterns::get_types_from_select;
+use crate::table::create_tables;
 use std::collections::HashMap;
 
-use sqlparser::{dialect::SQLiteDialect, parser::Parser};
-
-use crate::{expr::get_type_of_columns_from_select, table::create_table};
 mod expr;
+mod select_patterns;
 mod table;
+
 fn main() {
-    // let sql = "CREATE TABLE users (id INTEGER, name TEXT, wow REAL); ";
-    // let mut tables = HashMap::new();
-    // create_table(sql, &mut tables);
+    let mut all_tables = HashMap::new();
 
-    // Changed SQL to explicit columns because '*' is a Wildcard, not an Expr
-    let sql = "SELECT * FROM users ";
+    let sql = "CREATE TABLE users (id INTEGER, name TEXT, wow REAL)";
+    create_tables(sql, &mut all_tables);
 
-    // get_type_of_columns_from_select(sql, &tables);
+    let sql = "CREATE TABLE mom (id INTEGER, name TEXT, wow REAL)";
+    create_tables(sql, &mut all_tables);
 
-    let ast = &Parser::parse_sql(&SQLiteDialect {}, sql).unwrap()[0];
-    println!("{:#?}", ast);
+    let sql = "SELECT users.wow+ 1, mom.id / 3.0 AS asd FROM users, mom";
+    let types = get_types_from_select(sql, &all_tables);
+    println!("{:?}", types);
+
 }
--- type_inference/src/select_patterns.rs
+++ type_inference/src/select_patterns.rs
@@ -1,1 +1,58 @@
+use sqlparser::{
+    ast::{SelectItem, SetExpr, Statement},
+    dialect::SQLiteDialect,
+    parser::Parser,
+};
+use std::collections::HashMap;
 
+// Assuming these exist in your crate
+use crate::expr::evaluate_expr_type;
+use crate::{
+    expr::Type,
+    table::{ColumnInfo, get_table_names},
+};
+
+#[allow(unused)]
+pub fn get_types_from_select(
+    sql: &str,
+    all_tables: &HashMap<String, Vec<ColumnInfo>>,
+) -> Vec<Type> {
+    let dialect = SQLiteDialect {};
+    let table_names_from_select = get_table_names(sql);
+
+    let ast = Parser::parse_sql(&dialect, sql).unwrap();
+
+    let mut column_types = Vec::new();
+
+    if let Statement::Query(query) = &ast[0]
+        && let SetExpr::Select(select) = &*query.body
+    {
+        for item in &select.projection {
+            match item {
+                // SELECT column_name OR SELECT count(*)
+                SelectItem::UnnamedExpr(expr) => {
+                    let t = evaluate_expr_type(expr, table_names_from_select.clone(), all_tables);
+                    column_types.push(t);
+                }
+
+                // Case: SELECT column_name AS alias
+                SelectItem::ExprWithAlias { expr, alias: _ } => {
+                    let t = evaluate_expr_type(expr, table_names_from_select.clone(), all_tables);
+                    column_types.push(t);
+                }
+
+                // Case: SELECT *
+                SelectItem::Wildcard(_options) => {
+                    todo!();
+                }
+
+                // Case: SELECT users.*
+                SelectItem::QualifiedWildcard(object_name, _options) => {
+                    todo!();
+                }
+            }
+        }
+    }
+
+    column_types
+}
