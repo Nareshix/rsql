602b3f80413919419ac94444a5fa782654276772
Nareshix <nareshix66@gmail.com>
2025-11-27 16:52:23 +0800

    changed the var naming of test

--- type_inference/src/lib.rs
+++ type_inference/src/lib.rs
@@ -2,43 +2,43 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-mod select_result_type;
 mod expr;
+mod select_result_type;
 mod table;
 
 #[cfg(test)]
 mod tests {
-    use crate::expr::Type;
-    use crate::table::{FieldInfo, create_table};
+    use crate::expr::{BaseType, Type};
+    use crate::table::{ColumnInfo, create_tables};
     use std::collections::HashMap;
 
     // TODO simple test only not comprehensive do more creation of table
     #[test]
-    fn test_create_table_no_check_constraint() {
+    fn test_create_tables_no_check_constraint() {
         let sql = "CREATE TABLE users (id INTEGER, name TEXT, wow REAL); ";
         let mut tables = HashMap::new();
-        create_table(sql, &mut tables);
+        create_tables(sql, &mut tables);
 
         let mut expected = HashMap::new();
-        let fields = vec![
-            FieldInfo {
+        let columns = vec![
+            ColumnInfo {
                 name: "id".to_string(),
-                data_type: Type::Int,
+                data_type: Type { base_type: BaseType::Integer, nullable: false },
                 check_constraint: None,
             },
-            FieldInfo {
+            ColumnInfo {
                 name: "name".to_string(),
-                data_type: Type::String,
+                data_type: Type { base_type: BaseType::Text, nullable: false },
                 check_constraint: None,
             },
-            FieldInfo {
+            ColumnInfo {
                 name: "wow".to_string(),
-                data_type: Type::Float,
+                data_type: Type { base_type: BaseType::Real, nullable: false },
                 check_constraint: None,
             },
         ];
 
-        expected.insert("users".to_string(), fields);
+        expected.insert("users".to_string(), columns);
 
         assert_eq!(tables, expected);
     }
