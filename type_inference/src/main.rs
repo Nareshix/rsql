c349415b67ba50b2316d268962c918153bf50358
Nareshix <nareshix66@gmail.com>
2025-11-29 19:18:59 +0800

    tests

--- type_inference/src/main.rs
+++ type_inference/src/main.rs
@@ -2,10 +2,10 @@
 // use crate::table::create_tables;
 // use std::collections::HashMap;
 
+mod binding_patterns;
 mod expr;
 mod select_patterns;
 mod table;
-
 // fn main() {
 //     let mut all_tables = HashMap::new();
 
@@ -30,15 +30,10 @@ mod table;
 // }
 
 use crate::{
-    expr::evaluate_expr_type,
-    table::{create_tables, get_table_names},
+    binding_patterns::get_type_of_binding_parameters,
+    table::create_tables,
 };
-use sqlparser::{
-    ast::{Expr, Value, ValueWithSpan, visit_expressions},
-    dialect::SQLiteDialect,
-    parser::Parser,
-};
-use std::{collections::HashMap, ops::ControlFlow};
+use std::collections::HashMap;
 
 fn main() {
     let mut all_tables = HashMap::new();
@@ -49,52 +44,130 @@ fn main() {
     let sql = "CREATE TABLE mom (id INTEGER NOT NULL, name TEXT NOT NULL, wow TEXT)";
     create_tables(sql, &mut all_tables);
 
-    let sql = "Select 1 from users where name = ? AND wow + 1 > ? OR id in (?,?,?)";
-    let statements = Parser::parse_sql(&SQLiteDialect {}, sql).unwrap();
+    // let sql = "Select 1 from users where wow in (?,?,?)";
 
-    let table_names_from_select = get_table_names(sql);
-    let _ = visit_expressions(&statements, |expr| {
-        match expr {
-            Expr::BinaryOp { left, right, .. } => {
-                if let Expr::Value(ValueWithSpan { value, .. }) = &**right
-                    && let Value::Placeholder(_) = value
-                {
-                    let x =
-                        evaluate_expr_type(left, &table_names_from_select, &all_tables).unwrap();
-                    println!("{:?}", x);
-                }
-            }
-            Expr::InList { expr, list, .. } => {
-                // assume that all the InList contains ?
-                if let Expr::Value(ValueWithSpan { value, .. }) = &list[0]
-                    && let Value::Placeholder(_) = value
-                {
-                    let x =
-                        evaluate_expr_type(expr, &table_names_from_select, &all_tables).unwrap();
-                    println!("{:?}", x);
-                }
-            }
+    // let x = get_type_of_binding_parameters(sql, &all_tables);
+    // println!("{:?}", x);
 
-            Expr::Between {
-                expr, low, high, ..
-            } => {
-                if let Expr::Value(ValueWithSpan { value, .. }) = &**low
-                    && let Value::Placeholder(_) = value
-                {
-                    let x =
-                        evaluate_expr_type(expr, &table_names_from_select, &all_tables).unwrap();
-                    println!("{:?}", x);
-                } else if let Expr::Value(ValueWithSpan { value, .. }) = &**high
-                    && let Value::Placeholder(_) = value
-                {
-                    let x =
-                        evaluate_expr_type(expr, &table_names_from_select, &all_tables).unwrap();
-                    println!("{:?}", x);
-                }
-            }
-            _ => {}
-        }
+    // Test cases for SQL parameter binding detection
 
-        ControlFlow::<()>::Continue(())
-    });
+    let sql = "SELECT 1 FROM users AS u WHERE u.wow >= ?";
+    let x = get_type_of_binding_parameters(sql, &all_tables);
+    println!(" {:?}", x);
+
+    // let sql = "SELECT * FROM users WHERE id = ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "SELECT * FROM users WHERE name = ? AND wow > ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "SELECT * FROM users WHERE wow < ? OR id != ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "UPDATE users SET wow = ? WHERE id = ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "UPDATE users SET name = ?, wow = ? WHERE id > ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "INSERT INTO users (id, name, wow) VALUES (?, ?, ?)";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "INSERT INTO mom (id, name, wow) VALUES (?, ?, ?)";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "DELETE FROM users WHERE id = ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "DELETE FROM users WHERE name = ? AND wow <= ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "SELECT * FROM users WHERE id IN (?, ?, ?)";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "SELECT * FROM users WHERE wow >= ? AND wow <= ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "UPDATE mom SET name = ? WHERE id = ? AND wow = ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // // let sql = "SELECT * FROM mom WHERE name LIKE ?";
+    // // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // // println!("{} - {:?}", sql, x);
+
+    // let sql = "SELECT * FROM users WHERE name = ? OR name = ? OR name = ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "UPDATE users SET wow = wow + ? WHERE id = ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "SELECT * FROM users WHERE id > ? AND id < ? AND wow = ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // // let sql = "INSERT INTO users VALUES (?, ?, ?)";
+    // // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // // println!("{} - {:?}", sql, x);
+
+    // let sql = "DELETE FROM mom WHERE id >= ? AND name != ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "SELECT name FROM users WHERE wow IS NOT NULL AND id = ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "UPDATE mom SET wow = ? WHERE id IN (?, ?, ?)";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "SELECT * FROM users WHERE wow = ? AND name LIKE ? AND id > ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "DELETE FROM users WHERE wow BETWEEN ? AND ? AND name = ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "SELECT COUNT(*) FROM users WHERE id < ? AND wow > ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "UPDATE users SET name = ? WHERE wow IN (?, ?) AND id != ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "SELECT * FROM mom WHERE id = ? OR (name = ? AND wow = ?)";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "INSERT INTO mom (id, name) VALUES (?, ?)";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "SELECT * FROM users WHERE wow != ? AND id NOT IN (?, ?)";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "UPDATE users SET wow = ?, name = ? WHERE id BETWEEN ? AND ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
+
+    // let sql = "DELETE FROM mom WHERE wow = ? OR id = ?";
+    // let x = get_type_of_binding_parameters(sql, &all_tables).unwrap();
+    // println!("{} - {:?}", sql, x);
 }
