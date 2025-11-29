b87d5cf48139833ce98d5de8fd70a838a5574c5e
Nareshix <nareshix66@gmail.com>
2025-11-29 13:42:21 +0800

    tests

--- type_inference/src/main.rs
+++ type_inference/src/main.rs
@@ -1,10 +1,44 @@
-use crate::select_patterns::get_types_from_select;
-use crate::table::create_tables;
-use std::collections::HashMap;
+// use crate::select_patterns::get_types_from_select;
+// use crate::table::create_tables;
+// use std::collections::HashMap;
 
 mod expr;
 mod select_patterns;
 mod table;
+
+// fn main() {
+//     let mut all_tables = HashMap::new();
+
+//     let sql = "CREATE TABLE users (id INTEGER, name TEXT, wow REAL)";
+//     create_tables(sql, &mut all_tables);
+
+//     let sql = "CREATE TABLE mom (id INTEGER, name TEXT NOT NULL, wow TEXT)";
+//     create_tables(sql, &mut all_tables);
+
+//     let sql = "SELECT * FROM mom";
+//     let types = get_types_from_select(sql, &all_tables);
+//     println!("{:?}", types);
+
+//     let sql = "SELECT id > 'asd' FROM mom";
+//     let types = get_types_from_select(sql, &all_tables);
+//     println!("{:?}", types);
+
+//     let sql = "SELECT not NULL";
+//     let types = get_types_from_select(sql, &all_tables);
+//     println!("{:?}", types);
+
+// }
+
+use crate::{
+    expr::evaluate_expr_type,
+    table::{create_tables, get_table_names},
+};
+use sqlparser::{
+    ast::{Expr, Value, ValueWithSpan, visit_expressions},
+    dialect::SQLiteDialect,
+    parser::Parser,
+};
+use std::{collections::HashMap, ops::ControlFlow};
 
 fn main() {
     let mut all_tables = HashMap::new();
@@ -12,11 +46,55 @@ fn main() {
     let sql = "CREATE TABLE users (id INTEGER, name TEXT, wow REAL)";
     create_tables(sql, &mut all_tables);
 
-    let sql = "CREATE TABLE mom (id INTEGER, name TEXT, wow REAL)";
+    let sql = "CREATE TABLE mom (id INTEGER NOT NULL, name TEXT NOT NULL, wow TEXT)";
     create_tables(sql, &mut all_tables);
 
-    let sql = "SELECT users.wow+ 1, mom.id / 3.0 AS asd FROM users, mom";
-    let types = get_types_from_select(sql, &all_tables);
-    println!("{:?}", types);
+    let sql = "Select 1 from users where name = ? AND wow + 1 > ? OR id in (?,?,?)";
+    let statements = Parser::parse_sql(&SQLiteDialect {}, sql).unwrap();
 
+    let table_names_from_select = get_table_names(sql);
+    let _ = visit_expressions(&statements, |expr| {
+        match expr {
+            Expr::BinaryOp { left, right, .. } => {
+                if let Expr::Value(ValueWithSpan { value, .. }) = &**right
+                    && let Value::Placeholder(_) = value
+                {
+                    let x =
+                        evaluate_expr_type(left, &table_names_from_select, &all_tables).unwrap();
+                    println!("{:?}", x);
+                }
+            }
+            Expr::InList { expr, list, .. } => {
+                // assume that all the InList contains ?
+                if let Expr::Value(ValueWithSpan { value, .. }) = &list[0]
+                    && let Value::Placeholder(_) = value
+                {
+                    let x =
+                        evaluate_expr_type(expr, &table_names_from_select, &all_tables).unwrap();
+                    println!("{:?}", x);
+                }
+            }
+
+            Expr::Between {
+                expr, low, high, ..
+            } => {
+                if let Expr::Value(ValueWithSpan { value, .. }) = &**low
+                    && let Value::Placeholder(_) = value
+                {
+                    let x =
+                        evaluate_expr_type(expr, &table_names_from_select, &all_tables).unwrap();
+                    println!("{:?}", x);
+                } else if let Expr::Value(ValueWithSpan { value, .. }) = &**high
+                    && let Value::Placeholder(_) = value
+                {
+                    let x =
+                        evaluate_expr_type(expr, &table_names_from_select, &all_tables).unwrap();
+                    println!("{:?}", x);
+                }
+            }
+            _ => {}
+        }
+
+        ControlFlow::<()>::Continue(())
+    });
 }
