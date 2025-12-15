0e9823f8c225127a81152f576f73683851592661
Nareshix <Nareshix66@gmail.com>
2025-12-15 20:55:43 +0800

    cleaned up workspace. inline sql is on another branch and that may be done in future. or maybe not. idk :p

---
+++ tests/src/lib.rs
@@ -0,0 +1,45 @@
+use rsql::lazy_sql;
+
+#[lazy_sql]
+pub struct ShopDao {
+    create_table: sql!(
+        " CREATE TABLE Persons (
+        PersonID INTEGER NOT NULL,
+        LastName TEXT NOT NULL,
+        FirstName TEXT NOT NULL,
+        Address TEXT,
+        Alive INTEGER NOT NULL CHECK (Alive IN (0,1))
+    ) STRICT;"
+    ),
+
+    insert: sql!(
+        "INSERT INTO Persons (PersonID, LastName, FirstName, Address, Alive)
+        VALUES (1, 'Smith', 'hi', '123 Main Street', ?);"
+    ),
+
+    select: sql!("SELECT * FROM persons"),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rsql::LazyConnection;
+
+    #[test]
+    fn test_shop_flow() -> Result<(), Box<dyn std::error::Error>> {
+        // This is the code you had in main()
+        let conn = LazyConnection::open_memory().unwrap();
+        let mut dao = ShopDao::new(&conn);
+
+        dao.create_table()?;
+        dao.insert(true)?;
+
+        let results = dao.select()?;
+
+        let first_person = results.first()?.unwrap();
+
+        println!("{:?}", first_person);
+
+        Ok(())
+    }
+}
